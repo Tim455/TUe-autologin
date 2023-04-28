025563a7c0dc00a77e4719b6027f3a122358b5b4
Tim455 <113600754+Tim455@users.noreply.github.com>
2023-04-28 16:11:38 +0200

    Make username constistant and increase version number

--- header.js
+++ header.js
@@ -4,8 +4,8 @@
 // @match       https://sts.tue.nl/adfs/ls/
 // @match       https://login.microsoftonline.com/*
 // @grant       none
-// @version     1.0
-// @author      _Tim_
+// @version     1.1
+// @author      Tim455
 // @description 18/10/2022, 13:45:50
 // ==/UserScript==
 
